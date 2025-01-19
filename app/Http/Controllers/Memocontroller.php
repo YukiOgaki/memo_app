2181d2ac2e9c67ff96e461d3f47ca26970feed49
YukiOgaki <teamgift8@gmail.com>
2025-01-19 14:01:41 +0900

    一覧機能作成

--- app/Http/Controllers/Memocontroller.php
+++ app/Http/Controllers/Memocontroller.php
@@ -8,6 +8,13 @@ use Illuminate\Http\Request;
 class Memocontroller extends Controller
 {
     // showページへ移動
+    public function index()
+    {
+        $memos = Memo::all();
+        return view('memos.index', ['memos' => $memos]);
+    }
+
+    // showページへ移動
     public function show($id)
     {
         $memo = Memo::find($id);

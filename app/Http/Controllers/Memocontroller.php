55b01b639027b9a7922a2732abe0e281dfb56369
YukiOgaki <teamgift8@gmail.com>
2025-01-19 15:42:21 +0900

    登録機能作成

--- app/Http/Controllers/Memocontroller.php
+++ app/Http/Controllers/Memocontroller.php
@@ -7,11 +7,32 @@ use Illuminate\Http\Request;
 
 class Memocontroller extends Controller
 {
-    // showページへ移動
+    // indexページへ移動
     public function index()
     {
         $memos = Memo::all();
         return view('memos.index', ['memos' => $memos]);
+    }
+
+    // createページへ移動
+    public function create()
+    {
+        return view('memos.create');
+    }
+
+    // storeページへ移動
+    public function store(Request $request)
+    {
+        //インスタンスの作成
+        $memo = new Memo();
+
+        $memo->title = $request->title;
+        $memo->body = $request->body;
+
+        $memo->save();
+
+        //登録したらindexに戻る
+        return redirect(route('memos.index'));
     }
 
     // showページへ移動

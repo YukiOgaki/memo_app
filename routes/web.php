b3f376ee89a3ed2f45aa82280c5b8dfa55504d2c
YukiOgaki <teamgift8@gmail.com>
2025-01-23 10:41:58 +0900

    デプロイ設定

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Memocontroller;
 
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [MemoController::class, 'index']);
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Route::resource('memos', MemoController::class);
